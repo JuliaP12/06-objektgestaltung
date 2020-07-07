aadd51ba25868d1da56f1a991582eeb0ca1c9dac
JuliaP12 <67861836+JuliaP12@users.noreply.github.com>
2020-07-07 14:58:35 +0200

    Create 06_Objektgestaltung.dart

---
+++ 06_Objektgestaltung.dart
@@ -0,0 +1,14 @@
+class Essen{ 
+
+String geschmack;
+int menge;
+
+void zeigeWerteAn() { 
+print(this.geschmack);
+print(this.menge);
+}
+}
+Essen essen=Essen();
+essen.geschmack="Brötchen"
+essen.menge=Kuchen;
+essen.zeigeWerteAn
